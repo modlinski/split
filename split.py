5d36f55f39e1529147183cd7026ff5039f787455
modlinski <modlinski@modlinski-GL72-6QC>
2017-09-23 19:00:56 +0200

    First implementation without maxsplit parameter.

--- split.py
+++ split.py
@@ -12,107 +12,68 @@ def my_split(string, sep=None, maxsplit=-1):
 
     split_list = []
 
-    if sep == '':
-        raise ValueError
-
-    elif not sep or sep not in string:
+    if sep is None or sep not in string:
         split_list.append(string)
+
+    elif sep == '':
+        raise ValueError
 
     elif type(sep) != str:
         raise TypeError
 
     elif sep:
-        el = ''
-        ind = 0
-        loop_range = len(string)
-        while ind < loop_range:
-            if string[ind:ind + len(sep)] != sep:
-                el += string[ind]
-                ind += 1
-            if (string[ind:ind + len(sep)] == sep) or (ind == loop_range):
-                split_list.append(el)
-                el = ''
-                ind += len(sep)
-
+        end = False
+        while not end:
+            for i in range(len(string) - len(sep) + 1):
+                if string[i:i + len(sep)] == sep:
+                    first = string[:i]
+                    split_list.append(first)
+                    string = string[i+len(sep):]
+                    break
+            else:
+                split_list.append(string)
+                end = True
     return split_list
 
 
+# good practise is to add a few test cases before implementing a function
 class TestMySplit(unittest.TestCase):
 
-    # class variable using self like sleep
-    attribute = 'new'
-    string_1 = 'mic___hal___mod___lin___ski'
-    sep_1 = '___'
-    sep_2 = '__'
-    sep_3 = '_'
-    sep_4 = 'mic'
-    sep_5 = 'ski'
-    sep_6 = 'm'
-    sep_7 = 'i'
-    sep_8 = 'l_'
-    sep_9 = '_m'
-    sep_10 = ''
-    sep_11 = 'long---------------------------------string'
-    sep_12 = 100
-    sep_13 = 0  # important test case for this
+    def test_not_contains_separator(self):
+        self.assertEqual(my_split('abc___def___ghi___jkl', 'mno'), ['abc___def___ghi___jkl'])
 
-    # def __init__(self, methodName='runTest'):
-    #     super().__init__(methodName=methodName)
-    #
-    #     self.attribute = 'new'
-    #     self.string_1 = 'mic___hal___mod___lin___ski'
-    #     self.sep_1 = '___'
-    #     self.sep_2 = '__'
-    #     self.sep_3 = '_'
-    #     self.sep_4 = 'mic'
-    #     self.sep_5 = 'ski'
-    #     self.sep_6 = 'm'
-    #     self.sep_7 = 'i'
-    #     self.sep_8 = 'l_'
-    #     self.sep_9 = '_m'
-    #     self.sep_10 = ''
-    #     self.sep_11 = 'long---------------------------------string'
-    #     self.sep_12 = 100
-    #     self.sep_13 = 0  # important test case for this
+    def test_contains_part_of_separator(self):
+        self.assertEqual(my_split('abc___def___ghi___jkl', '____'), ['abc___def___ghi___jkl'])
 
-    def test_1(self):
-        self.assertEqual(my_split(self.string_1, self.sep_1), self.string_1.split(self.sep_1))
+    def test_contains_separator(self):
+        self.assertEqual(my_split('abc___def___ghi___jkl', '___'), ['abc', 'def', 'ghi', 'jkl'])
 
-    def test_2(self):
-        self.assertEqual(my_split(self.string_1, self.sep_2), self.string_1.split(self.sep_2))
+    def test_contains_two_separators(self):
+        self.assertEqual(my_split('abc___def___ghi___jkl', '__'), ['abc', '_def', '_ghi', '_jkl'])
 
-    def test_3(self):
-        self.assertEqual(my_split(self.string_1, self.sep_3), self.string_1.split(self.sep_3))
+    def test_contains_three_separators(self):
+        self.assertEqual(my_split('abc___def___ghi___jkl', '_'), ['abc', '', '', 'def', '', '', 'ghi', '', '', 'jkl'])
 
-    def test_4(self):
-        self.assertEqual(my_split(self.string_1, self.sep_4), self.string_1.split(self.sep_4))
+    def test_first_one_letter(self):
+        self.assertEqual(my_split('abc___def___cba___fed', 'a'), ['', 'bc___def___cb', '___fed'])
 
-    # def test_5(self):
-    #     self.assertEqual(my_split(self.string_1, self.sep_5), self.string_1.split(self.sep_5))
+    def test_last_one_letter(self):
+        self.assertEqual(my_split('abc___def___cba___fed', 'd'), ['abc___', 'ef___cba___fe', ''])
 
-    def test_6(self):
-        self.assertEqual(my_split(self.string_1, self.sep_6), self.string_1.split(self.sep_6))
+    def test_no_parameters(self):
+        self.assertEqual(my_split('abc___def___ghi___jkl'), ['abc___def___ghi___jkl'])
 
-    # def test_7(self):
-    #     self.assertEqual(my_split(self.string_7, self.sep_7), self.string_7.split(self.sep_7))
+    def test_separator_longer_than_string(self):
+        self.assertEqual(my_split('abc_def', 'longer_than_string'), ['abc_def'])
 
-    def test_8(self):
-        self.assertEqual(my_split(self.string_1, self.sep_8), self.string_1.split(self.sep_8))
+    def test_value_error_empty_string(self):
+        self.assertRaises(ValueError, my_split, 'mic___hal___mod___lin___ski', '')
 
-    def test_9(self):
-        self.assertEqual(my_split(self.string_1, self.sep_9), self.string_1.split(self.sep_9))
+    def test_type_error_int_one_hundred(self):
+        self.assertRaises(TypeError, my_split, 'mic___hal___mod___lin___ski', 100)
 
-    # def test_10(self):
-        # self.assertRaises(ValueError, my_split(self.string_1, self.sep_10))
-
-    def test_11(self):
-        self.assertEqual(my_split(self.string_1), self.string_1.split())
-
-    def test_12(self):
-        self.assertEqual(my_split(self.string_1, self.sep_11), self.string_1.split(self.sep_11))
-
-    # def test_13(self):
-        # self.assertRaises(TypeError, my_split(self.string_1, self.sep_12))
+    def test_type_error_int_zero(self):
+        self.assertRaises(TypeError, my_split, 'mic___hal___mod___lin___ski', 0)
 
 if __name__ == '__main__':
     unittest.main()
